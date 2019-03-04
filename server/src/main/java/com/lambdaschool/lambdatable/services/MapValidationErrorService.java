56e875087042f4e7ed5c81ba16c705ecf1809a76
Harrison Brock <harrison@harrisonbrock.com>
2019-03-04 18:40:40 -0500

    added error map handling to make error messages easier to read

---
+++ server/src/main/java/com/lambdaschool/lambdatable/services/MapValidationErrorService.java
@@ -0,0 +1,28 @@
+package com.lambdaschool.lambdatable.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> mapValidationService(BindingResult result) {
+
+        if (result.hasErrors()) {
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
