14cbfe5f5b5326432249f272c2eede586294e4ef
Harrison Brock <harrison@harrisonbrock.com>
2019-03-04 18:42:09 -0500

    Added resource not found exception. This will make more user friendly message when the user tries to access a resource not in the database.

---
+++ server/src/main/java/com/lambdaschool/lambdatable/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.lambdatable.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
