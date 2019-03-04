fe3c70df21c70457fda202549668907b17f7a0ff
Harrison Brock <harrison@harrisonbrock.com>
2019-03-04 12:31:14 -0500

    start of project

---
+++ server/src/main/java/com/lambdaschool/lambdatable/LambdaTableApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.lambdatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaTableApplication.class, args);
+    }
+
+}
