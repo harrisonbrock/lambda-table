7655dc748e7ec6b1ec60347c7287419f18a67046
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-05 12:39:33 -0600

    Wireframe end of week

--- client/src/components/eow-form.js
+++ client/src/components/eow-form.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-// import './Eod.css';
+// import './Eow.css';
 
 class Eow extends Component {
     render() {
@@ -10,6 +10,23 @@ class Eow extends Component {
                     <header className="table-header">
                         <img src={logo} className="App-logo" alt="logo" />
                     </header>
+                    <div>
+                        <h3>Describe this week in 3 words</h3>
+                        <textarea />
+                    </div>
+                    <div>
+                        <h3>What went well this week?</h3>
+                        <textarea />
+                    </div>
+                    <div>
+                        <h3>What could have gone better?</h3>
+                        <textarea />
+                    </div>
+                    <div>
+                        <h3>What did you work on this week?</h3>
+                        <textarea />
+                    </div>
+                    <button>Submit</button>
                 </div>
             </div>
         )
