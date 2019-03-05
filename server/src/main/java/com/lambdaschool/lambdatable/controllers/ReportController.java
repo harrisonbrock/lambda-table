28cb96bd3efdf940cf8668e678362accf361fcfa
Harrison Brock <harrison@harrisonbrock.com>
2019-03-05 18:37:08 -0500

    updated user to have roles, username and password

--- server/src/main/java/com/lambdaschool/lambdatable/controllers/ReportController.java
+++ server/src/main/java/com/lambdaschool/lambdatable/controllers/ReportController.java
@@ -1,10 +1,12 @@
 package com.lambdaschool.lambdatable.controllers;
 
 import com.lambdaschool.lambdatable.model.Report;
+import com.lambdaschool.lambdatable.services.MapValidationErrorService;
 import com.lambdaschool.lambdatable.services.ReportService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +15,25 @@ import javax.validation.Valid;
 @RequestMapping(value = "/reports", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ReportController {
 
-//    private final ReportService reportService;
+    private final ReportService reportService;
+    private final MapValidationErrorService errorService;
+
+    public ReportController(ReportService reportService, MapValidationErrorService errorService) {
+        this.reportService = reportService;
+        this.errorService = errorService;
+    }
+
+    @PostMapping("/users/{gitHubUserName}")
+    public ResponseEntity<?> createNewReport(
+            @PathVariable String gitHubUserName,
+            @Valid @RequestBody Report report,
+            BindingResult result) {
+
+        ResponseEntity<?> errorMap = errorService.mapValidationService(result);
+
+        if (errorMap != null) return errorMap;
+        Report newReport = reportService.createReport(report, gitHubUserName);
+        return new ResponseEntity<>(newReport, HttpStatus.CREATED);
+    }
 
 }
