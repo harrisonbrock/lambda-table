bf5e99a4616fbd39037693cf64e9519b9868974d
Harrison Brock <harrison@harrisonbrock.com>
2019-03-05 18:34:27 -0500

    refactor method name

--- server/src/main/java/com/lambdaschool/lambdatable/repositories/UserRepository.java
+++ server/src/main/java/com/lambdaschool/lambdatable/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByGitHubUserName(String gitHubName);
+    Optional<User> findByGitHubName(String gitHubName);
+    Optional<User> findByName(String name);
 }
