02046d027dd0b939d7b2b292194673182ab65af4
Harrison Brock <harrison@harrisonbrock.com>
2019-03-05 18:37:15 -0500

    updated user to have roles, username and password

--- server/src/main/java/com/lambdaschool/lambdatable/model/User.java
+++ server/src/main/java/com/lambdaschool/lambdatable/model/User.java
@@ -1,24 +1,57 @@
 package com.lambdaschool.lambdatable.model;
 
+import com.lambdaschool.lambdatable.model.audit.DateAudit;
 import lombok.Data;
+import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
-@Table(name = "users")
-public class User {
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "gitHubName"
+        }),
+        @UniqueConstraint(columnNames = {
+                "email"
+        })
+})
+public class User extends DateAudit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userid")
-    private Long Id;
+    private Long id;
 
     @NotEmpty
     private String name;
 
-    @Column(name = "githubusername")
+    @NotBlank
+    @Size(max = 15)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    @Column(name = "gitHubName")
     @NotEmpty
-    private String gitHubUserName;
+    private String gitHubName;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
