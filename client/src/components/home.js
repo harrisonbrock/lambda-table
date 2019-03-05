c1cb46d4ea2e3125c09d35281df58c6376f8cef1
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-04 18:18:51 -0600

    Some initialization routing

---
+++ client/src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="overall-div">
+                <div className="centered-content-div">
+                    <header className="table-header">
+                        <h1>Home</h1>
+                    </header>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
