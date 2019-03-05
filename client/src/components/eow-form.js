e39fc59a105fbc859ba9e0ac53bc7f37c037033f
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-05 14:52:52 -0600

    inputs tied to state

--- client/src/components/eow-form.js
+++ client/src/components/eow-form.js
@@ -3,6 +3,17 @@ import logo from '../logo.svg';
 // import './Eow.css';
 
 class Eow extends Component {
+    state = {
+        threeWords: "",
+        wentWell: "",
+        goneBetter: "",
+        workedOn: ""
+    }
+
+    onInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
     render() {
         return (
             <div className="overall-div">
@@ -12,19 +23,19 @@ class Eow extends Component {
                     </header>
                     <div>
                         <h3>Describe this week in 3 words</h3>
-                        <textarea />
+                        <textarea value={this.state.threeWords} name="threeWords" onChange={this.onInputChange} />
                     </div>
                     <div>
                         <h3>What went well this week?</h3>
-                        <textarea />
+                        <textarea value={this.state.wentWell} name="wentWell" onChange={this.onInputChange} />
                     </div>
                     <div>
                         <h3>What could have gone better?</h3>
-                        <textarea />
+                        <textarea value={this.state.goneBetter} name="goneBetter" onChange={this.onInputChange} />
                     </div>
                     <div>
                         <h3>What did you work on this week?</h3>
-                        <textarea />
+                        <textarea value={this.state.workedOn} name="workedOn" onChange={this.onInputChange} />
                     </div>
                     <button>Submit</button>
                 </div>
