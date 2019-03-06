db7d6345182ccebc47bfda2a819c3487e04e6517
Harrison Brock <harrison@harrisonbrock.com>
2019-03-06 13:21:56 -0500

    updated user model

--- server/src/main/java/com/lambdaschool/lambdatable/model/User.java
+++ server/src/main/java/com/lambdaschool/lambdatable/model/User.java
@@ -13,8 +13,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
 @Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "username"
+        }),
         @UniqueConstraint(columnNames = {
                 "gitHubName"
         }),
@@ -54,4 +56,71 @@ public class User extends DateAudit {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String name, String username, String email, String password, String gitHubName) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.gitHubName = gitHubName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGitHubName() {
+        return gitHubName;
+    }
+
+    public void setGitHubName(String gitHubName) {
+        this.gitHubName = gitHubName;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
