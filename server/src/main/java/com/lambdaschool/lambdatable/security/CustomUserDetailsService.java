0592b3482dea4792737ad8dd36c0f7756f72e382
Harrison Brock <harrison@harrisonbrock.com>
2019-03-06 13:25:14 -0500

    added security classes

---
+++ server/src/main/java/com/lambdaschool/lambdatable/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.lambdaschool.lambdatable.security;
+
+import com.lambdaschool.lambdatable.exception.ResourceNotFoundException;
+import com.lambdaschool.lambdatable.model.User;
+import com.lambdaschool.lambdatable.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String usernameOrEmail)
+            throws UsernameNotFoundException {
+        // Let people login with either username or email
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email : " + usernameOrEmail)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("")
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
