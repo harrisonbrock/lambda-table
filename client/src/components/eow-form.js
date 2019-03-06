bfec76cf925a358137d94e1bd4e7a15d32508a62
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-03-05 16:48:40 -0800

    Added className for calling in logo

--- client/src/components/eow-form.js
+++ client/src/components/eow-form.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
-// import './Eow.css';
+// import logo from '../logo.svg';
+import './Eow.css';
 
 class Eow extends Component {
     state = {
@@ -19,7 +19,8 @@ class Eow extends Component {
             <div className="overall-div">
                 <div className="centered-content-div">
                     <header className="table-header">
-                        <img src={logo} className="App-logo" alt="logo" />
+                        <div className="logo" />
+                        {/* <img src={CloudChart} className="logo" alt="CloudChart" /> */}
                     </header>
                     <div>
                         <h3>Describe this week in 3 words</h3>
