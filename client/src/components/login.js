a2cc33629b9bfaee8df4a995833a07d958c22306
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-07 02:12:18 -0600

    Prettier-fied, updated signup to default to user dashboard

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -19,7 +19,12 @@ class Login extends React.Component {
 			<div className="wrapper">
 				<h1>Log In</h1>
 				<span>Register</span>
-				<input name="register" type="checkbox" checked={this.state.register} onChange={this.handleCheck} />
+				<input
+					name="register"
+					type="checkbox"
+					checked={this.state.register}
+					onChange={this.handleCheck}
+				/>
 				<form onSubmit={this.loginHandler}>
 					<div>
 						<label htmlFor="username" />
@@ -34,36 +39,42 @@ class Login extends React.Component {
 					</div>
 					<div>
 						<label htmlFor="name" />
-						{this.state.register ? <input
-							name="name"
-							placeholder="name"
-							value={this.state.name}
-							onChange={this.inputChangeHandler}
-							type="text"
-							className="text-entry"
-						/> : null}
+						{this.state.register ? (
+							<input
+								name="name"
+								placeholder="name"
+								value={this.state.name}
+								onChange={this.inputChangeHandler}
+								type="text"
+								className="text-entry"
+							/>
+						) : null}
 					</div>
 					<div>
 						<label htmlFor="github" />
-						{this.state.register ? <input
-							name="github"
-							placeholder="github"
-							value={this.state.github}
-							onChange={this.inputChangeHandler}
-							type="text"
-							className="text-entry"
-						/> : null}
+						{this.state.register ? (
+							<input
+								name="github"
+								placeholder="github"
+								value={this.state.github}
+								onChange={this.inputChangeHandler}
+								type="text"
+								className="text-entry"
+							/>
+						) : null}
 					</div>
 					<div>
 						<label htmlFor="email" />
-						{this.state.register ? <input
-							name="email"
-							placeholder="email"
-							value={this.state.email}
-							onChange={this.inputChangeHandler}
-							type="email"
-							className="text-entry"
-						/> : null}
+						{this.state.register ? (
+							<input
+								name="email"
+								placeholder="email"
+								value={this.state.email}
+								onChange={this.inputChangeHandler}
+								type="email"
+								className="text-entry"
+							/>
+						) : null}
 					</div>
 					<div>
 						<label htmlFor="password" />
@@ -76,11 +87,11 @@ class Login extends React.Component {
 							className="text-entry"
 						/>
 					</div>
-					<button className="login-button">{this.state.register ? "Register" : "Log in"}</button>
+					<button className="login-button">
+						{this.state.register ? "Register" : "Log in"}
+					</button>
 				</form>
-				{this.state.invalidCredentials ? (
-					<h3>Invalid Credentials</h3>
-				) : null}
+				{this.state.invalidCredentials ? <h3>Invalid Credentials</h3> : null}
 			</div>
 		);
 	}
@@ -91,25 +102,32 @@ class Login extends React.Component {
 
 	handleCheck = e => {
 		this.setState({ register: !this.state.register });
-	}
+	};
 
 	loginHandler = event => {
 		event.preventDefault();
 
 		if (!this.state.register) {
-			Axios
-				.post("http://localhost:8080/api/auth/signin",
-					{ userNameOrEmail: this.state.username, password: this.state.password })
+			Axios.post("http://localhost:8080/api/auth/signin", {
+				userNameOrEmail: this.state.username,
+				password: this.state.password
+			})
 				.then(response => {
 					if (response.data.accessToken) {
 						localStorage.setItem("token", response.data.accessToken);
 						// this.props.history.push("/eow");
 						Axios.get("http://localhost:8080/api/admin/reports", {
-							headers: { Authorization: "Bearer " + localStorage.getItem("token") }
-						}).then(res => {
-							res.data === [] ? this.props.history.push("/admin") : this.props.history.push("/user");
-							console.log(res)
-						}).catch(err => console.log(err));
+							headers: {
+								Authorization: "Bearer " + localStorage.getItem("token")
+							}
+						})
+							.then(res => {
+								res.data === []
+									? this.props.history.push("/admin")
+									: this.props.history.push("/user");
+								console.log(res);
+							})
+							.catch(err => console.log(err));
 					}
 				})
 				.catch(err => {
@@ -118,19 +136,17 @@ class Login extends React.Component {
 					this.setState({ invalidCredentials: true, password: "" });
 				});
 		} else {
-			Axios
-				.post("http://localhost:8080/api/auth/signup",
-					{
-						userName: this.state.username,
-						password: this.state.password,
-						name: this.state.name,
-						gitHubName: this.state.github,
-						email: this.state.email
-					})
+			Axios.post("http://localhost:8080/api/auth/signup", {
+				userName: this.state.username,
+				password: this.state.password,
+				name: this.state.name,
+				gitHubName: this.state.github,
+				email: this.state.email
+			})
 				.then(response => {
 					if (response.data.accessToken) {
 						localStorage.setItem("token", response.data.accessToken);
-						this.props.history.push("/eow");
+						this.props.history.push("/user");
 					}
 				})
 				.catch(err => {
