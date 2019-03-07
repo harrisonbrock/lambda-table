caa876dcaa6808855a44d94cabbbb5b04720fa6d
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-07 01:22:55 -0600

    redirects to login if no token is present

--- client/src/components/eow-form.js
+++ client/src/components/eow-form.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
 import './Eow.css';
 
 class Eow extends Component {
@@ -7,42 +6,50 @@ class Eow extends Component {
 		threeWords: "",
 		wentWell: "",
 		goneBetter: "",
-		workedOn: ""
+		workedOn: "",
+		projectURL: ""
 	}
 
 	onInputChange = e => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 
-    render() {
-        return (
-            <div className="overall-div">
-                <div className="centered-content-div">
-                    <header className="table-header">
-                        <div className="logo" />
-                        {/* <img src={CloudChart} className="logo" alt="CloudChart" /> */}
-                    </header>
-                    <div>
-                        <h3>Describe this week in 3 words</h3>
-                        <textarea value={this.state.threeWords} name="threeWords" onChange={this.onInputChange} />
-                    </div>
-                    <div>
-                        <h3>What went well this week?</h3>
-                        <textarea value={this.state.wentWell} name="wentWell" onChange={this.onInputChange} />
-                    </div>
-                    <div>
-                        <h3>What could have gone better?</h3>
-                        <textarea value={this.state.goneBetter} name="goneBetter" onChange={this.onInputChange} />
-                    </div>
-                    <div>
-                        <h3>What did you work on this week?</h3>
-                        <textarea value={this.state.workedOn} name="workedOn" onChange={this.onInputChange} />
-                    </div>
-                    <button>Submit</button>
-                </div>
-            </div>
-        )
-    }
+	componentDidMount() {
+		if (!localStorage.getItem("token")) this.props.history.push("/login");
+	}
+
+	render() {
+		return (
+			<div className="overall-div">
+				<div className="centered-content-div">
+					<header className="table-header">
+						<div className="logo" />
+					</header>
+					<div>
+						<h3>Describe this week in 3 words</h3>
+						<textarea value={this.state.threeWords} name="threeWords" onChange={this.onInputChange} className="textbox" />
+					</div>
+					<div>
+						<h3>What went well this week?</h3>
+						<textarea value={this.state.wentWell} name="wentWell" onChange={this.onInputChange} className="textbox" />
+					</div>
+					<div>
+						<h3>What could have gone better?</h3>
+						<textarea value={this.state.goneBetter} name="goneBetter" onChange={this.onInputChange} className="textbox" />
+					</div>
+					<div>
+						<h3>What did you work on this week?</h3>
+						<textarea value={this.state.workedOn} name="workedOn" onChange={this.onInputChange} className="textbox" />
+					</div>
+					<div>
+						<h3>What is your project submission URL?</h3>
+						<textarea value={this.state.projectURL} name="projectURL" onChange={this.onInputChange} className="textbox" />
+					</div>
+					<button className="submit-btn">Submit</button>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Eow;
