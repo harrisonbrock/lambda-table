7c823419c6ff56997a41f5e482867c40933a83bc
Cruise Brantley <cruise.brantley@gmail.com>
2019-03-07 19:18:53 -0600

    Now accepts data from cards to update as well

--- client/src/components/eow-form.js
+++ client/src/components/eow-form.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './Eow.css';
+import React, { Component } from "react";
+import "./Eow.css";
 
 class Eow extends Component {
 	state = {
@@ -7,16 +7,27 @@ class Eow extends Component {
 		wentWell: "",
 		goneBetter: "",
 		workedOn: "",
-		projectURL: ""
-	}
-
-   onInputChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-
+		projectURL: "",
+		importedState: false
+	};
 	componentDidMount() {
 		if (!localStorage.getItem("token")) this.props.history.push("/login");
+		if (this.props.location.state !== undefined) {
+			this.setState({
+				threeWords: this.props.location.state.report.describeWeek || "",
+				wentWell: this.props.location.state.report.whatWentWellThisWeek || "",
+				goneBetter:
+					this.props.location.state.report.whatCouldHaveWentBetter || "",
+				workedOn:
+					this.props.location.state.report.whatDidYouWorkOnThisWeek || "",
+				projectURL: this.props.location.state.report.urlSubmission || "",
+				importedState: true
+			}); //if state is sent in from component calling eow
+		}
 	}
+	onInputChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
 	render() {
 		return (
@@ -27,28 +38,53 @@ class Eow extends Component {
 					</header>
 					<div>
 						<h3>Describe this week in 3 words</h3>
-						<textarea value={this.state.threeWords} name="threeWords" onChange={this.onInputChange} className="textbox" />
+						<textarea
+							value={this.state.threeWords}
+							name="threeWords"
+							onChange={this.onInputChange}
+							className="textbox"
+						/>
 					</div>
 					<div>
 						<h3>What went well this week?</h3>
-						<textarea value={this.state.wentWell} name="wentWell" onChange={this.onInputChange} className="textbox" />
+						<textarea
+							value={this.state.wentWell}
+							name="wentWell"
+							onChange={this.onInputChange}
+							className="textbox"
+						/>
 					</div>
 					<div>
 						<h3>What could have gone better?</h3>
-						<textarea value={this.state.goneBetter} name="goneBetter" onChange={this.onInputChange} className="textbox" />
+						<textarea
+							value={this.state.goneBetter}
+							name="goneBetter"
+							onChange={this.onInputChange}
+							className="textbox"
+						/>
 					</div>
 					<div>
 						<h3>What did you work on this week?</h3>
-						<textarea value={this.state.workedOn} name="workedOn" onChange={this.onInputChange} className="textbox" />
+						<textarea
+							value={this.state.workedOn}
+							name="workedOn"
+							onChange={this.onInputChange}
+							className="textbox"
+						/>
 					</div>
 					<div>
 						<h3>What is your project submission URL?</h3>
-						<textarea value={this.state.projectURL} name="projectURL" onChange={this.onInputChange} className="textbox" />
+						<textarea
+							value={this.state.projectURL}
+							name="projectURL"
+							onChange={this.onInputChange}
+							className="textbox"
+						/>
 					</div>
 					<button className="submit-btn">Submit</button>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
