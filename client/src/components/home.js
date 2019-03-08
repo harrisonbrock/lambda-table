0dba12302777fb40236c41cd289fe6f653ef85a6
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-03-07 16:12:17 -0800

    Added styling to home pg; and cloud puff mascot

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./login.css";
 
 class Home extends Component {
 	render() {
 		return (
-			<div className="overall-div">
+			<div className="wrapper">
+			{/* <div className="overall-div"> */}
 				<div className="centered-content-div">
 					<header className="table-header">
-						<h1>Home</h1>
-						<Link to="/eow">End of Week Form</Link>
+					<div className="logo" />
+						<h1 className="login">Home</h1>
+						<Link to="/eow" className="eowForm">End of Week Form</Link>
 						<br />
-						<Link to="/login">Login</Link>
+						<Link to="/login" className="log-in">Login</Link>
 					</header>
 				</div>
+				<div className="puff" />
 			</div>
 		);
 	}
