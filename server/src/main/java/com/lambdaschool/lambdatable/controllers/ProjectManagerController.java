c61218d0942f3a2d08f21f36d697a30d5fd7a6b9
Harrison Brock <harrison@harrisonbrock.com>
2019-03-07 19:07:12 -0500

    Added Get all Project Manages

---
+++ server/src/main/java/com/lambdaschool/lambdatable/controllers/ProjectManagerController.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.lambdatable.controllers;
+
+import com.lambdaschool.lambdatable.repositories.ProjectManagerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(name = "/api/pm")
+public class ProjectManagerController {
+
+    private final ProjectManagerRepository projectManagerRepository;
+
+    public ProjectManagerController(ProjectManagerRepository projectManagerRepository) {
+        this.projectManagerRepository = projectManagerRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProjectManagers() {
+        return new ResponseEntity<>(projectManagerRepository.findAll(), HttpStatus.OK);
+    }
+}
